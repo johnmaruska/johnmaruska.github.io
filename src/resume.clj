7c5a87415dd8b1e0e4bf26fb4aa6c7a1ea562eee
John Maruska <jackmaruska@gmail.com>
2023-09-20 14:06:11 -0500

    Switch to Kat's CSS

--- src/resume.clj
+++ src/resume.clj
@@ -13,87 +13,89 @@
    [:meta {:name "keywords" :content (map name (:meta-keywords basics))}]
    [:meta {:name "viewport" :content "initial-scale=1,width=device-width"}]
    [:meta {:content "no-cache" :http-equiv "cache-control"}]
-   [:link {:href "styles.css" :rel "stylesheet" :type "text/css"}]
+   [:link {:rel "stylesheet" :href "styles.css" :type "text/css"}]
+   [:link {:rel "stylesheet" :href "https://fonts.googleapis.com/css?family=Space+Mono|Muli"}]
+   [:link {:rel "stylesheet" :href "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"}]
    [:title (str (:name basics) ". " (:label basics))]])
 
-(defn title [{:keys [basics]}]
-  [:div.title
+(defn introduction [{:keys [basics]}]
+  (list
    [:h1.name (:name basics)]
    [:p.role (:label basics)]
-   [:p.location (:location basics)]])
+   [:p.location (:location basics)]))
 
 (def pdf-link
-  [:a.pdf-link {:href "resume.pdf"}
-   [:img {:src "img/pdf-icon.svg"}]])
+  [:a.pdf-link {:href "resume.pdf"} [:i {:class "fa fa-file-pdf-o"}]])
 
-(defn contact-links [{:keys [basics]}]
-  [:ul.contact-links
-   (for [profile (:contact-links basics)]
-     [:li
-      [:a {:href (:url profile)}
-       [:img {:src (:icon profile)}]
-       (:network profile)]])])
+(defn links [{:keys [basics]}]
+  (for [profile (:contact-links basics)]
+    [:a {:href (:url profile)}
+     [:i {:class (str "fa fa-" (:icon profile))}]]))
 
 (defn summary [{:keys [basics]}]
-  [:div.summary
+  (list
    [:h2 "Summary"]
-   [:p (:content (:summary basics))]])
+   [:p.summary (:content (:summary basics))]))
+
+(defn education [{:keys [education]}]
+  (list
+   [:h2 (:title education)]
+   [:ul.education
+    (for [entry (:content education)]
+      [:li
+       [:p
+        (str (:study-type entry) " in " (:area entry))
+        [:br] [:a {:href (:website entry)} (:institution entry)]]])]))
+
+(defn today [] (.format (java.text.SimpleDateFormat. "MMMM d, YYYY") (java.util.Date.)))
+(defn updated []
+  [:div.updated [:p (str "updated:" (today))]])
+
+(defn sidebar [{:keys [basics] :as resume-data}]
+  [:div.sidebar
+   [:div.sidebar-content
+    (introduction resume-data)
+    (links resume-data)
+    pdf-link
+    [:hr.divider]
+    (summary resume-data)
+    (education resume-data)]
+   (updated)])
 
 ;; dates in EDN are given in [yyyy "Month" dd] format
 (defn date [[year month day]]
   (str month " " year))
 (defn interval [{:keys [start end]}]
   (if (nil? end)
-    [:p  (date start)]
-    (list
-     [:p (date start) " to " (date end)])))
-
-(defn experience-li [entry]
-  [:li.company
-   [:div.summary
-    [:a.name {:href (:website entry)} (:company entry)]
-    [:div.role (:position entry)]
-    [:div.interval (interval entry)]
-    [:div.keywords
-     [:p (string/join ", " (map name (:keywords entry)))]]]
-   [:div.details
-    [:p (or (:highlight entry) "TODO")]
-    [:ul (for [bullet (:bullets entry)]
-           [:li bullet])]]])
+    (date start)
+    (str (date start) " to " (date end))))
 
 (defn experience [{:keys [fulltime-experience]}]
-  [:div.experience
+  (list
    [:h2 (:title fulltime-experience)]
    [:ul.experience
     (for [entry (:content fulltime-experience)]
-      (experience-li entry))]])
-
-(defn education [{:keys [education]}]
-  [:div.education
-   [:h2 (:title education)]
-   [:ul
-    (for [entry (:content education)]
-      [:li
-       [:p
-        (str (:study-type entry) " in " (:area entry) " at ")
-        [:a {:href (:website entry)} (:institution entry)]]])]])
-
-(defn today [] (.format (java.text.SimpleDateFormat. "MMMM d, YYYY") (java.util.Date.)))
-(defn updated []
-  [:div.updated-date (str "updated:" (today))])
+      [:li.company
+       [:a.name {:href (:website entry)} (:company entry)]
+       [:p.position (:position entry)]
+       [:p.interval (interval entry)]
+       [:p.keywords
+        [:p (string/join ", " (map name (:keywords entry)))]]
+       #_ [:div.details
+           [:p (or (:highlight entry) "TODO")]
+           [:ul.details (for [bullet (:bullets entry)]
+                          [:li bullet])]]
+       [:ul.details (for [bullet (:bullets entry)]
+                      [:li bullet])]])]))
 
 (defn render [resume-data]
   (html5
    (head resume-data)
    [:body
-    pdf-link
-    [:div.header
-     (title resume-data)
-     (contact-links resume-data)]
-    (summary resume-data)
-    (experience resume-data)
-    (education resume-data)
-    (updated)]))
+    (sidebar resume-data)
+    [:div.main
+     [:div.main-content
+      (experience resume-data)]]]))
 
 (defn page []
   (render (edn/read-string (slurp (io/resource "experience.edn")))))
