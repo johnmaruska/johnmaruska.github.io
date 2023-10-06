0e57cd5c467d7e94b480a9e98a2b55b9ceee9d87
John Maruska <jackmaruska@gmail.com>
2023-10-06 17:34:58 -0500

    Add picture to links page

--- src/links.clj
+++ src/links.clj
@@ -8,16 +8,22 @@
   (html5
    [:head
     ;; Shared
-    [:meta {:name "viewport" :content "initial-scale=1,width=device-width"}]
-    [:meta {:content "no-cache" :http-equiv "cache-control"}]
-    [:link {:rel "stylesheet" :href "https://fonts.googleapis.com/css?family=Space+Mono|Muli"}]
-    [:script {:src "https://kit.fontawesome.com/729e48c2e5.js" :crossorigin "anonymous"}]
+    [:meta {:name "viewport"
+            :content "initial-scale=1,width=device-width"}]
+    [:meta {:content "no-cache"
+            :http-equiv "cache-control"}]
+    [:link {:rel "stylesheet"
+            :href "https://fonts.googleapis.com/css?family=Space+Mono|Muli"}]
+    [:script {:src "https://kit.fontawesome.com/729e48c2e5.js"
+              :crossorigin "anonymous"}]
     ;; Unique
     [:link {:rel "stylesheet" :href "links.css" :type "text/css"}]
     [:title "John Maruska - Links"]]
    [:body
     [:div.column
      [:div#links-profile
+      [:img#avatar {:src "profile_picture_512x521.png"
+                    :alt "profile pic of John. Long hair, full beard, glasses."}]
       [:p "These are all the accounts I associate with my real name. I've been full hermit/lurker mode on most of these but I'm hoping to change that soonish."]]
      [:ul#links
       (for [{:keys [network url icon]} links-data]
